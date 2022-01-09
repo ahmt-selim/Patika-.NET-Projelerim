f0e818a3837dfae366d81ac3683d797a5311c951
Ahmet Selim <ahmt.selim12@gmail.com>
2022-01-09 22:45:57 +0300

    Merhaba dünya projemi yükledim

---
+++ Merhaba Dünya/Merhaba Dünya/Merhaba Dünya/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Merhaba_Dünya
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Merhaba Dünya!");
+            string isim, soyisim;
+            Console.Write("İsminizi girin: ");
+            isim=Convert.ToString(Console.ReadLine());
+            Console.Write("Soyisminizi girin: ");
+            soyisim=Convert.ToString(Console.ReadLine());
+            Console.WriteLine("Merhaba "+isim+" "+soyisim);
+
+            Console.ReadLine();
+        }
+    }
+}
